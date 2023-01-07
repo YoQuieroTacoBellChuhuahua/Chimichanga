a657935131f569489f5e9403713938b9a94e59bd
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-01-07 12:25:09 +0200

    Create Mamavinga.js

---
+++ Mamavinga.js
@@ -0,0 +1,13 @@
+function phoneBook(input) {
+
+    let phonebook = {};
+    for (let line of input) {
+    let tokens = line.split(' ');
+    let name = tokens[0];
+    let number = tokens[1];
+    phonebook[name] = number;
+    }
+    for (let key in phonebook) {
+    console.log(`${key} -> ${phonebook[key]}`);
+    }
+    }
